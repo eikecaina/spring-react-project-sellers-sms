d2523419b690d54bd0644184c41ef5ad13e3c3aa
Eike Hank <cainaeike@hotmail.com>
2022-11-15 18:17:42 -0300

    Other components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import NotificationButton from './components/NotificationButton'
+import Header from './components/Header'
+import SalesCard from './components/SalesCard'
 
 
 function App() {
   return (
     <>
-      <h1>Olá mundo</h1>
-      <NotificationButton/>
-    </> 
+      <Header />
+      <main>
+        <section id="sales">
+          <div className="dsmeta-container">
+            <SalesCard/>
+          </div>
+        </section>
+      </main>
+    </>
   )
 }
 
---
+++ frontend/src/components/Header/index.tsx
@@ -0,0 +1,18 @@
+import logo from '../../assets/img/logo.svg'
+import './styles.css'
+
+
+function Header() {
+    return (
+        <header>
+            <div className="dsmeta-logo-container">
+                <img src={logo} alt="DSMeta" />
+                <h1>DSMeta</h1>
+            </div>
+        </header >
+
+    )
+}
+
+export default Header
+
